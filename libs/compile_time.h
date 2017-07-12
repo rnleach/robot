c5da4a39ece4c74670683363258a4d7e936109df
Ryan Leach <ryan.n.leach@gmail.com>
2017-07-12 10:58:42 -0600

    Added IS_SET macro for bits/registers.

--- libs/compile_time.h
+++ libs/compile_time.h
@@ -12,9 +12,12 @@ Author: Ryan Leach
 #define STRINGIFY(s) STRINGIFY_(s)
 #define STRINGIFY_(s) #s
 
-// Set and clear bits in a register.
+// Set and clear bits in a register. (Same code works on any variable.)
 #define SET_BIT(register, bit) ((register) |= (1<<(bit)))
 #define CLEAR_BIT(register, bit) ((register) &= ~(1<<(bit)))
+
+// Check if a bit is set or cleared in a register. (Same code works on any variable.)
+#define IS_SET(register, bit) ((register) & (1<<(bit)))
 
 // Constant for the processor clock speed, should be overridden with -D CFLAG in the makefile.
 #ifndef F_CPU
